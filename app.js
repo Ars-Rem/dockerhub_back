7ec79ec16285b2ff804321eb6c34fb6e1ae6fe62
Ars-Rem <1800gsm@gmail.com>
2021-09-07 14:48:53 +0300

    n

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = express()
 var server = require('http').createServer(app)
 var port = 4730;
 var io = require('socket.io')(server, {
-  origins: 'localhost:3001',
+  origins: 'localhost:3000',
 })
 
 var { onconnect } = require('./services/sockets/websocket.service')
