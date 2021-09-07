9ad818d543b770e4de99ca02cb9d69b9579e629b
Ars-Rem <1800gsm@gmail.com>
2021-09-07 16:02:24 +0300

    up

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var server = require('http').createServer(app)
 var port = 4730;
 var io = require('socket.io')(server, {
   origins: 'http://194.44.111.70:4731',
-  //origins: 'localhost:3000'
+ 
 })
 
 var { onconnect } = require('./services/sockets/websocket.service')
