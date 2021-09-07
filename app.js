e49466132866b40e7e67e7d5fd4958912490d0a1
Ars-Rem <1800gsm@gmail.com>
2021-09-07 15:10:37 +0300

    merged

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ var app = express()
 var server = require('http').createServer(app)
 var port = 4730;
 var io = require('socket.io')(server, {
-  //origins: 'http://194.44.111.70:4731',
-  origins: 'localhost:3000'
+  origins: 'http://194.44.111.70:4731',
+  //origins: 'localhost:3000'
 })
 
 var { onconnect } = require('./services/sockets/websocket.service')
